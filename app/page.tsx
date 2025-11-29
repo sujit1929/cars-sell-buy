9c5d88a36071efa5c1e3225e1b1912ec96053946
sujit1929 <sujit1505v@gmail.com>
2025-11-29 15:34:18 +0530

    `Refactor HomeGemini page and its components`

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ import HomeGemini from "@/components/SystemUI/Home/GeminiSection"
 export default function Home() {
   return (
     <>
-      {/* <NavigationMenuDemo />
-      <HeroSection/>
-      <ThemeToggle/> */}
       <HomeGemini />
     </>
   )
